cc662cd5357b8f0b41ff9deb06e5411fc8ea8f91
YoonHwe <dbsghl676@gmail.com>
2024-07-02 00:16:41 +0900

    Random 클래스

---
+++ lang/math/MathMain.java
@@ -0,0 +1,19 @@
+package lang.math;
+
+public class MathMain {
+
+    public static void main(String[] args) {
+        // ㄱㅣ본 연산 메서드
+        //max, min, abs
+
+        //반올림 및 정밀도 메서드
+        //Math.ceil 올림
+        //Math.floor 내림
+        //Math.round 반올림
+
+        //기타 유용한 메서드
+        //sqrt 제곱근
+        //random : 0.0 ~ 1.0 사이의 double 값을 리턴
+        System.out.println(Math.random());
+    }
+}
